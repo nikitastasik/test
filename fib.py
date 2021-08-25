58fe11d68f7ffa9918d9f6ed931bc292e54f2073
nikitastasik <nikitastasik7321@gmail.com>
2021-08-25 12:31:52 +0300

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,47 +1,32 @@
-# def fib():
-#
-#     if n == 1:
-#         return 1
-#     a, b = 0, 1
-#     for i in range(2, n+1):
-#         a, b = b, a + b
-#         # print(b)
-#     f_n = b
-#
-#     if f_n // m == 1:
-#         return 1
-#     elif f_n // m > 1:
-#         ost = f_n // m
-#         return f_n - (ost * m)
-# print(fib())
+
 from Tools.demo.beer import n
 
-# n, m = map(int, input().split())
-#
-# def fib(n, m):
-#     pis = [0]
-#     if m == 1:
-#         return pis
-#     pis.append(1)
-#     if n <= 1:
-#         return pis
-#     n_0 = 0
-#     n_1 = 1
-#     print(f'origin pis {pis}')
-#     for __ in range(m * 6):
-#         n_0, n_1 = n_1, (n_0+n_1) % m
-#         print(f'n_0 = {n_0} n_1 = {n_1} m = {m}')
-#         pis.append(n_1 % m)
-#         print(pis)
-#
-#
-#
-#     return pis
-#
-# res = fib(n, m)
-#
-#
-# print(res[n % len(res)])
+n, m = map(int, input().split())
+
+def fib(n, m):
+    pis = [0]
+    if m == 1:
+        return pis
+    pis.append(1)
+    if n <= 1:
+        return pis
+    n_0 = 0
+    n_1 = 1
+    print(f'origin pis {pis}')
+    for __ in range(m * 6):
+        n_0, n_1 = n_1, (n_0+n_1) % m
+        print(f'n_0 = {n_0} n_1 = {n_1} m = {m}')
+        pis.append(n_1 % m)
+        print(pis)
+
+
+
+    return pis
+
+res = fib(n, m)
+
+
+print(res[n % len(res)])
 
 
 log(2, (log(2, n))
